00d97f55668874b8a6d623d7ddd4e4043cfcb764
DESKTOP-BGP888H\ldj <lbj@cudo.co.kr>
2022-03-25 09:37:35 +0900

    기존 서버 작업 목록 추가

---
+++ backend/src/main/java/com/acinlee/backend/api/controller/SessionController.java
@@ -0,0 +1,103 @@
+package com.acinlee.backend.api.controller;
+
+import com.acinlee.backend.api.service.TestService;
+import com.acinlee.backend.api.entity.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+@RestController
+public class SessionController {
+    private static final String MEMBER_KEY = "memberKey";
+
+    @Autowired
+    TestService testService;
+
+    @RequestMapping("/user/session/login")
+    public int login(@RequestParam String userId, @RequestParam String userPw, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+
+        Cookie [] cookies = request.getCookies();
+        boolean isSid = false;
+        if(cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if ("sid".equals(cookie.getName())) {
+                    isSid = true;
+                }
+            }
+        }
+
+        if(isSid) {
+            if( session != null && session.getAttribute("userSid") != null ){
+                if (userId.equals(session.getAttribute("id"))){
+                    if (userPw.equals(session.getAttribute("pw"))) {
+                        response.setStatus(200);
+                    } else {
+                        response.setStatus(400);
+                    }
+                }
+            }
+        } else {
+            Random random = new Random();
+            String userSid = "A" + random.nextInt(9999999);
+            session.setAttribute("userSid", userSid);
+            session.setAttribute("id", userId);
+            session.setAttribute("pw", userPw);
+            // 쿠키 생성
+            Cookie cookie = new Cookie("sid", userSid);
+            cookie.setDomain("localhost");
+            cookie.setPath("/");
+            cookie.setMaxAge(30);
+            response.addCookie(cookie);
+            response.setStatus(400);
+        }
+        return response.getStatus();
+    }
+
+    @PostMapping("/user/join")
+    public ResponseEntity<TestEntity> userJoin(@RequestBody TestEntity test){
+        testService.join(test.getUserId(), test.getUserPw());
+        return new ResponseEntity<>(test, HttpStatus.OK);
+    }
+
+    @RequestMapping("/user/login")
+    public ResponseEntity userLogin(@RequestParam String userId, @RequestParam String userPw) {
+        boolean isReturn = testService.login(userId, userPw);
+        if (isReturn) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @RequestMapping("/session/create")
+    public String sessionCreate(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        System.out.println(session.toString());
+        String sessionString = "helloSessionWorld";
+        session.setAttribute("helloSession", sessionString);
+
+        // cookie 생성
+
+        return session.getId();
+    }
+
+    @RequestMapping("/session/confirm")
+    public String sessionConfirm(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        System.out.println(session.getAttribute("helloSession"));
+        if(session.getAttribute("helloSession") != null) {
+            return "success";
+        } else{
+            return "false";
+        }
+    }
+}
---
+++ backend/src/main/java/com/acinlee/backend/api/service/TestService.java
@@ -0,0 +1,43 @@
+package com.acinlee.backend.api.service;
+
+import com.acinlee.backend.api.entity.TestEntity;
+import com.acinlee.backend.api.repository.TestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+    private final TestRepository testRepository;
+
+    public TestEntity join(String userId, String userPw) {
+        TestEntity test = TestEntity.builder()
+                .userId(userId)
+                .userPw(userPw)
+                .build();
+        return testRepository.save(test);
+    }
+
+    public boolean login(String userId, String userPw) {
+        TestEntity test = testRepository.findById(userId).orElse(null);
+        if( test != null ) {
+            if ( (userPw).equals(test.getUserPw())) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public Optional<TestEntity> getUser(String userId) {
+        Optional<TestEntity> testObject = testRepository.findById(userId);
+
+        return testRepository.findById(userId);
+    }
+}
